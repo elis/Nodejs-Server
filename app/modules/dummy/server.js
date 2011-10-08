47c58449969d665754dee70ad4653e017037ae39
Eli Sklar <eli.sklar@gmail.com>
2011-10-08 02:38:19 +0200

    added modules and dummy module

---
+++ app/modules/dummy/server.js
@@ -0,0 +1,13 @@
+// Server
+moduleColor = "#4E6394";
+
+var links = Module.links = Module.sections = {
+	'testing': {
+		title: 'Test Number #1',
+		section: true
+	},
+	'another-test': {
+		title: 'Another Test',
+		section: true
+	}
+};
